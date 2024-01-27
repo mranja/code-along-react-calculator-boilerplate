cabb505e452373e3e02c549b15277f6219ded7e8
ranjan <ranjan.m@kalvium.community>
2024-01-27 10:29:23 +0530

    Done

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import DisplayCalculator from "./Component/DisplayCalculator";
+import "./App.css"; 
+
+function App() {
+  return (
+    <>
+      <DisplayCalculator />
+    </>
+  );
+}
+
+export default App;
